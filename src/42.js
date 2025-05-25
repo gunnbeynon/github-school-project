30ce79cff8e062ba61e41faf8a730ef2b268ae73
gunnbeynon <gunnbeynon@gmail.com>
2025-05-25 18:58:21 +0000

    Add 42.js

---
+++ src/42.js
@@ -0,0 +1,8 @@
+let express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
